ef11ec3f2e1e516bcbd726f895be34f24956476e
feizheng <1290657123@qq.com>
2016-10-31 10:00:43 +0800

    update centos

--- index.sh
+++ index.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
+
 ## path:
 ROOT_PATH=$(dirname $BASH_SOURCE);
+
 alias sys-version='cat /proc/version';
 alias sys-info='uname -a';
 alias sys-hostname='sudo vim /etc/sysconfig/network';
+
+
+if [ -f /etc/redhat-release ]; then
+  source $ROOT_PATH/index_centos.sh;
+fi
 
 
 unset ROOT_PATH;
