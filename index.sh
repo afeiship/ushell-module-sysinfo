d1bb3360ca771fb07575c1bdf8683fb323002723
afeiship <1290657123@qq.com>
2018-04-27 21:49:30 +0800

    issue: sys-memory

--- index.sh
+++ index.sh
@@ -6,6 +6,7 @@ ROOT_PATH=$(dirname $BASH_SOURCE);
 alias sys-version='cat /proc/version';
 alias sys-info='uname -a';
 alias sys-hostname='sudo vim /etc/sysconfig/network';
+alias sys-memory='free -m';
 
 
 if [ -f /etc/redhat-release ]; then
